6a34784501365aa2609c04d31e22bce09ff23c80
PEEKAYbyte <pnkpnk121@gmail.com>
2024-11-11 15:59:46 +0530

    before note cred operation

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,19 @@
+var jwt = require("jsonwebtoken");
+const JWT_SCRET = "jNE LEJ JELA";
+
+const fetchuser = (req, res, next) => {
+  // get user from jwt token add id
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ error: "Please authenticate with vaalid token" });
+  }
+  try {
+    const data = jwt.verify(token, JWT_SCRET);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.error(401).send({error:"please authenticate using a valid token"})
+  }
+};
+
+module.exports = fetchuser;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs"); 
+const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
+var fetchuser =require('../middleware/fetchuser');
 
-const JWT_SCRET = "jNE LEJ JELA"
+const JWT_SCRET = "jNE LEJ JELA";
 
 // create a user using : POST "/api/auth/createuser", no login requireddoesnt required auth
 
@@ -14,6 +15,8 @@ const JWT_SCRET = "jNE LEJ JELA"
 
 // change get to post
 //router.get('/', (req,res)=>{
+
+//router 1
 router.post(
   "/createuser",
   [
@@ -50,27 +53,90 @@ router.post(
         name: req.body.name,
         password: SecPass,
       });
-      const data ={
-        user:{
-          id:user.id
-        }
-      }
+
+      // data get user id as a authenticate
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
 
       //data store in authtoken
       const authtoken = jwt.sign(data, JWT_SCRET);
       // console.log(jwtData);
-      res.json({authtoken})
-
+      res.json({ authtoken });
 
       // res.json(user);
       //data send es6 authtoken
-
-
       // .then((user) => res.json(user))
       // res.send(req.body);
     } catch (error) {
       console.log(error.message);
       res.status(500).send("some error occured");
+    }
+  }
+);
+
+//..route 2..........................login .........
+
+// authenticate a user using post "/api/auth/login"  nno login required
+router.post(
+  "/login",
+  [
+    body("email", "enter a valid email name").isEmail(),
+    body("password", "password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are error return request and the error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array });
+    }
+
+    // email and password ae req.body mathi bahar kathie match kare.
+    const { email, password } = req.body;
+    // try catch use kari ne email match karshe,
+    try {
+      let user = await User.findOne({ email });
+      // email khoto hase to error aavshe
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "please try to correct credencials" });
+      }
+      // password ne compare karshe, encoded password sathe
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "please try to correct credencials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SCRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("internal server  error occured");
+    }
+  }
+);
+//...................step 3 data fetch........
+
+// route 3 : get loged in user detail using :POST"/api/auth/getuser" . login required
+router.post('/getuser',fetchuser, async (req, res) => {
+
+
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId).select("-password");
+      res.send(user)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("internal step3  server error ");
     }
   }
 );
