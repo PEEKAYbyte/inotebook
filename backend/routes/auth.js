14233001dfbde441bc185e2f34f808dbe385f37f
PEEKAYbyte <pnkpnk121@gmail.com>
2024-11-11 10:12:50 +0530

    without jwt authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 // create a user using : POST "/api/auth/createuser", no login requireddoesnt required auth
 
@@ -10,31 +11,49 @@ const { body, validationResult } = require("express-validator");
 
 // change get to post
 //router.get('/', (req,res)=>{
-router.post("/createuser",[
-    body("email", "enter a valid email name").isEmail(),
+router.post(
+  "/createuser",
+  [
     body("name", "enter valid name bhai saab").isLength({ min: 3 }),
-    body("password").isLength({ min: 5 }),
+    body("email", "enter a valid email name").isEmail(),
+    body("password", "password must beatleast 5 character long").isLength({
+      min: 5,
+    }),
   ],
   async (req, res) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //this is extra  // console.log(req.body);
-    // const user=User(req.body);
-    // user.save()
+    try {
+      //check whether the user with this email already exist
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "same email id match" });
+      }
 
-    let user = await User.create({
-      email: req.body.email,
-      name: req.body.name,
-      password: req.body.password,
-    })
-    
-    .then((user) => res.json(user))
-    // res.send(req.body);
-    .catch(error=> {console.log(error)
-    res.json({error:"please enter a unique email",message:error.message})})
-  });
+      //this is extra  // console.log(req.body);
+      // const user=User(req.body);
+      // user.save()
+      //bcryptjs use karva maate password ma change kariu che.
+      const salt = await bcrypt.genSalt(10);
+      // password ma salt add kare che.
+      // npm i jsonwebtoken install for token
+      const SecPass = await bcrypt.hash(req.body.password, salt);
 
+      // create a new user
+      user = await User.create({
+        email: req.body.email,
+        name: req.body.name,
+        password: SecPass,
+      });
+      res.json(user);
+      // .then((user) => res.json(user))
+      // res.send(req.body);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
 module.exports = router;
