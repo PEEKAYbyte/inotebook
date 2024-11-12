46bedd20b5368a9e43015990c99edb731ffbb35a
PEEKAYbyte <pnkpnk121@gmail.com>
2024-11-12 11:03:56 +0530

    update commit delete baki

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,7 +6,7 @@ var fetchuser = require("../middleware/fetchuser");
 const { body, validationResult } = require("express-validator");
 const Notes = require("../models/Notes");
 
-// route 1 GET ALL NOTE: using GET''/api/auth/getuser. login required
+// route 1 GET ALL NOTE: using GET''/api/notes/getuser. login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     // find notes
@@ -18,7 +18,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 //..............route 2
-// route 2 add new NOTE: using POST''/api/auth/addnote. login required
+// route 2 add new NOTE: using POST''/api/notes/addnote. login required
 router.post(
   "/addnote",
   fetchuser,
@@ -55,4 +55,63 @@ router.post(
   }
 );
 
-module.exports = router
+// route 3 update an existing note..POST api/notes/update. login required
+// same user find by id and same person can change the note
+// we use POST or PUT
+router.put(
+  "/updatenote/:id",
+  fetchuser,
+  // [body('title','Pleas enter a valid title').isLength({min:3}),
+  // body('description','description must be atleast 5 character long').isLength({min:5}),],
+  async (req, res) => {
+    // create a new object
+    const newNote = {};
+    const {title,description,tag}=req.body;
+    if(title){newNote.title=title};
+    if(description){newNote.description=description};
+    if(tag){newNote.tag=tag};
+
+    // find the note to be updated
+
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+// check loged user is same or not...
+    if(note.user && note.user.toString() !== req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id,{$set: newNote}, {new:true})
+    res.json({note});
+  }
+);
+
+//.......................route 4 ..........
+// route 4 delete an existing note..DELETE api/notes/deletenote. login required
+// same user find by id and same person can not delete the note
+// we use DELETE
+router.delete(
+  "/deletenote/:id",
+  fetchuser,
+  // [body('title','Pleas enter a valid title').isLength({min:3}),
+  // body('description','description must be atleast 5 character long').isLength({min:5}),],
+  async (req, res) => {
+    // create a new object
+    const newNote = {};
+    const {title,description,tag}=req.body;
+    if(title){newNote.title=title};
+    if(description){newNote.description=description};
+    if(tag){newNote.tag=tag};
+
+    // find the note to be updated
+
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+// check loged user is same or not...
+    if(note.user && note.user.toString() !== req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id,{$set: newNote}, {new:true})
+    res.json({note});
+  }
+);
+
+module.exports = router;
