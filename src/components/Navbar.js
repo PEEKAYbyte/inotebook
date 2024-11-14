86fa1537bea8717eab0461ef3b6cbc7eeab4c187
PEEKAYbyte <pnkpnk121@gmail.com>
2024-11-14 15:33:35 +0530

    all client edit update delete is done, api call pending

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 
@@ -7,9 +7,9 @@ const Navbar = () => {
   // this use effect use for change the navbar focus color when is activa or not
   // {`nav-link ${location.pathname==="/about"? "active":""}`}  use this for class name 
   let location = useLocation();
-  useEffect(()=>{
-    console.log(location.pathname);
-  },[location]);
+  // useEffect(()=>{
+  //   console.log(location.pathname);
+  // },[location]);
   return (
     
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
 
 const Noteitem = (props) => {
+  const context = useContext(noteContext);
   const { note } = props;
+  const {deleteNote} = context;
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">
-            {note.description} Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Rerum ut cum est voluptas, dignissimos pariatur
-            velit reiciendis cupiditate possimus architecto, deleniti impedit
-            voluptatibus vitae dolores sequi odit expedita itaque! Explicabo.
-          </p>
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">{note.title}</h5>
+            <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}}></i>
+            <i className="fa-solid fa-pen-to-square mx-2"></i>
+          </div>
+
+          <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
